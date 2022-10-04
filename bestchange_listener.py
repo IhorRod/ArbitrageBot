8fe1915ce6b301adb25c000bc9602c651137f663
Ihor Rodionov <108577029+IhorRod@users.noreply.github.com>
2022-10-04 13:33:56 +0300

    Update bestchange_listener.py

--- bestchange_listener.py
+++ bestchange_listener.py
@@ -17,7 +17,7 @@ def run_bestchange():
 async def run_bestchange1():
     try:
         await asyncio.get_event_loop().run_in_executor(None, update_cots)
-    except Exception:
+    except:
         traceback.print_exc()
         run_bestchange()
 
